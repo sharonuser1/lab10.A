1f512289ea003f2b895f17d4eb531f98ae9b9a18
Sharon Ogbanga <SHARON.OGBANGA.2024@mumail.ie>
2023-12-11 11:49:29 +0000

    copied the code given

---
+++ Question1.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Question1 {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Q1();
+        scan.close();
+    }
+
+    public static void Q1() {
+        while (true) {
+            System.out.println("Pick a shape: square, rectangle, circle (or 'q' to quit)");
+            String input = scan.nextLine();
+            if (input.equals("q")) {
+                return;
+            }
+
+            if (input.equals("square")) {
+                double a;
+                System.out.println("Enter the length of side a: ");
+                a = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the square is: " + a * 4);
+                System.out.println("The area of the square is: " + a * a);
+
+            } else if (input.equals("rectangle")) {
+                double a, b;
+                System.out.println("Enter the length of side a: ");
+                a = Double.parseDouble(scan.nextLine());
+                System.out.println("Enter the length of side b: ");
+                b = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the rectangle is: " + (2 * a + 2 * b));
+                System.out.println("The area of the rectangle is: " + (a * b));
+            } else if (input.equals("circle")) {
+                double r;
+                System.out.println("Enter the radius: ");
+                r = Double.parseDouble(scan.nextLine());
+                System.out.println("The circumference of the circle is: " + (Math.PI * r * 2));
+                System.out.println("The area of the circle is: " + (Math.PI * r * r));
+            }
+        }
+    }
+}
+
+
+    
+
+  
+
+
+    
+
